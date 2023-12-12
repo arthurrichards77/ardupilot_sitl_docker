7dbd0487f6029a0edbf0de39e64d68b9c41235e2
Arthur Richards <aeagr@bristol.ac.uk>
2023-12-12 15:07:55 +0000

    All working and responsive.  Much better than UDP version

--- stacks/three_copters_tcp/app/gw_launch.sh
+++ stacks/three_copters_tcp/app/gw_launch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mavlink-routerd -p $(getent ahostsv4 copter_1 | head -1 | awk '{print $1}'):5760 -p $(getent ahostsv4 copter_2 | head -1 | awk '{print $1}'):5760
+mavlink-routerd -p $(getent ahostsv4 copter_1 | head -1 | awk '{print $1}'):5760 -p $(getent ahostsv4 copter_2 | head -1 | awk '{print $1}'):5760 -p $(getent ahostsv4 copter_3 | head -1 | awk '{print $1}'):5760
